d054abebcf92f16f1d377135ac7fa2c45077e83f
jtlaughton <60359046+jtlaughton@users.noreply.github.com>
2022-01-24 21:44:01 -0600

    Finished 04_02

---
+++ Ch04/04_02/using-usestate/src/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App(){
+  const [status, setStatus] = useState("Open")
+  return (
+    <div>
+      <h1>Status: {status}</h1>
+      <button onClick= {() => {
+        setStatus("Closed");
+      }}>
+        Closed
+      </button>
+      <button onClick= {() => {
+        setStatus("Open");
+      }}>
+        Open
+      </button>
+      <button onClick= {() => {
+        setStatus("Back in Five");
+      }}>
+        Back in Five
+      </button>
+    </div>
+  );
+}
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
